a991d04ef90ba1dc51a83b8383da17f7a65a9e16
Remix Workspace <>
2022-10-14 16:42:05 +0530

    Updated PriceConverter importing with correct statement of "using PriceConverter for uint256"

--- Contracts/FundMe.sol
+++ Contracts/FundMe.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.0;
 import "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;
     uint256 public limitAmount = 50 * 10 ** 18;
     address[] public senders;
     mapping(address => uint256) public senderAndAmountSent;
 
     function fund() public payable {
-        require(getConversionRate(msg.value) >= limitAmount, "Didn't send enough");
+        require(msg.value.getConversionRate() >= limitAmount, "Didn't send enough");
         senders.push(msg.sender);
-        senderAndAmountSent[msg.sender] = msg.value;
+        senderAndAmountSent[msg.sender] =msg.value;
 
     }
 
