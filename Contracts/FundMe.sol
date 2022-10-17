e05a020048b0b13f4bde363273770041303a21da
Remix Workspace <>
2022-10-17 16:22:14 +0530

    Added Custom Errors and Receive/Fallback functionalities in case if someone send funds without using Fund method.

--- Contracts/FundMe.sol
+++ Contracts/FundMe.sol
@@ -3,22 +3,48 @@
 pragma solidity ^0.8.0;
 import "./PriceConverter.sol";
 
+
 contract FundMe{
     using PriceConverter for uint256;
     uint256 public limitAmount = 50 * 10 ** 18;
-    address[] public senders;
-    mapping(address => uint256) public senderAndAmountSent;
+    address[] public funders;
+    mapping(address => uint256) public funderAndAmountSent;
+    address public owner;
+    error unWantedResult();
+    constructor(){
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= limitAmount, "Didn't send enough");
-        senders.push(msg.sender);
-        senderAndAmountSent[msg.sender] =msg.value;
+        funders.push(msg.sender);
+        funderAndAmountSent[msg.sender] =msg.value;
 
     }
 
-    
+    function widthdraw() public {
+        for(uint256 funderIndex = 0; funderIndex< funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            funderAndAmountSent[funder] = 0;
+        }
 
-    // function widthdraw() public {
+        funders = new address[](0);
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess == true, "Call Failed");
+    }
 
-    // }
+    modifier onlyOwner {
+        // require(owner == msg.sender, "You're not Owner for Widthdraw Funds.");
+        if(owner == msg.sender){
+            revert unWantedResult();
+        }
+        _;
+    }
+    receive() external payable{
+        fund();
+    }
+
+    fallback() external {
+        fund();
+    }
 }
